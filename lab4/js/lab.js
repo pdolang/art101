7ab793a1119e6a119cfba62ed278533483e27c2c
pdolang <pdolang@ucsc.edu>
2023-10-20 10:08:31 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,32 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - The purpose of this JS file is to practice Pseudocoding for Lab 4 in ART101.
+// Author: Priscella Dolang <pdolang@ucsc.edu>
+// Date: 10/20/2023
 
-// Constants
+// Everyday Task - Making an Acrylic Cat Painting
 
-// Functions
+// 1. Gather your materials - paints, paint brushes, cup of water, towels, etc
+// 2. Prep your painting surface (canvas, paper, etc) by priming with white gesso 
+// 3. After the gesso has dried, mix a color that will be the undertone of your painting - this is for 
+// uniformity to create a color scheme guideline for your painting 
+// 4. Mix the color with water to dilute it and create a wash on your painting surface 
+// 5. Using your brush and color of your choice - make light and soft brush strokes to begin building the shape of your cat
+// 6. Mixing colors as needed - begin blocking in generalized shapes to start filling in the cat and other objects you may 
+// want to include
+// 7. After creating your desired cat shape - mix a darker shade of the cat’s body color and begin blocking in shadows to 
+// create a sense of depth and volume 
+// 8. Mix a lighter shade of the cat’s body color and begin blocking in the highlights to create a solidified light source to 
+// further accentuate the cat’s depth and volume
+// 9. Once you have your basic shadows and highlights (using whatever technique of painting you desire), use a smaller brush 
+// (or dry ish brush) to create fur-like strokes and give the illusion of the cat having fur (unless its hairless …) 
+// 10. After adding all details you deem necessary, allow paint to fully dry
+// 11. Bonus Step - you can seal your painting by adding a varnish (any finish) with a large brush on top once it is fully dry 
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Simple Computer Game - Rock Paper Scissors
 
-// let's get this party started
-main();
+// 1. Create simple layout to showcase user’s move and computer’s move
+// 2. Create 3 different options - Rock, Paper, & Scissors
+// 3. Ask user what option they pick 
+// 4. Create a randomized computer output either being Rock, Paper, or Scissors regardless of user input
+// 5. Showcase computer output and user output
+// 6. Determine win, draw, or loss result of the game 
