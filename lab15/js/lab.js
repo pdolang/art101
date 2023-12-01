f43bef747f08267a7ea931de6f3e3a2abaf02295
pdolang <pdolang@ucsc.edu>
2023-12-01 10:16:29 -0800

    lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -3,30 +3,28 @@
 // Date: 12/3/23
 
 // Credit to Wes Modes Canvas Page for Lab 15
-
-$.ajax({
-    // The URL for the request (from the api docs)
-    url: "",
-    // The data to send (will be converted to a query string)
-    data: {
-        // Here is where any data required by the api 
-        // goes (check the api docs)
-        id:
-        api_key: ""
-    };
-    // Whether this is a POST or GET request
-    type: "",
-    // The type of data we expect back
-    dataType: "",
-    // What do we do when the api call is successful
-    // All the action goes in here
-    success: function(data) {
-        // Do stuff
-        console.log(data);
-    },
-    // What do we do if the api call fails
-    error: function(jqXHR, textStatus, errorThrown) {
-        // Do stuff
-        console.log("Error: ", textStatus, errorThrown)
-    }
-})
+$("#activate").click(function() {
+    $.ajax({
+        // The URL for the request (from the api docs)
+        url: "https://dog.ceo/api/breeds/image/random",
+        // Whether this is a POST or GET request
+        type: "GET",
+        // The type of data we expect back
+        dataType: "json",
+        // What do we do when the api call is successful
+        // All the action goes in here
+        success: function(data) {
+            // Do stuff
+            // API has "image" property - message only showed in console 
+            var image = data.message;
+            // Update src attribute with img element with id "dogImage"
+            $("#dogImage").attr("src", image);
+            console.log(data);
+        },
+        // What do we do if the api call fails
+        error: function(jqXHR, textStatus, errorThrown) {
+            // Do stuff
+            console.log("Error: ", textStatus, errorThrown)
+        }
+    });
+});
