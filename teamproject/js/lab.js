bc76285dd81f7d94fa66a1d63019378b2684b432
pdolang <pdolang@ucsc.edu>
2023-12-10 17:00:47 -0800

    project

--- teamproject/js/lab.js
+++ teamproject/js/lab.js
@@ -1,27 +1,161 @@
+// JavaScript for Final Project 
 
+// Opening Screen
+$(function() {
+    var welcomeSection = $('.welcome-section'),
+        enterButton = welcomeSection.find('.enter-button'),
+        welcomeGif = welcomeSection.find('.welcome-gif');
 
-document.addEventListener("DOMContentLoaded", function () {
-    const colorChangeBtn = document.getElementById("colorChangeBtn");
+    setTimeout(function() {
+        welcomeSection.removeClass('content-hidden');
+    }, 500);
 
-    colorChangeBtn.addEventListener("click", changeColors);
+    enterButton.on('click', function(e) {
+        e.preventDefault();
 
-    function changeColors() {
-        const body = document.body;
-        const h1 = document.querySelector("h1");
+        // Hide the loading text and "Enter" button immediately when "Enter" is clicked
+        welcomeSection.addClass('show-gif');
+        welcomeSection.find('.fly-in-text li, .enter-button').hide(); // Hide the loading text and "Enter" button
 
-        // Example color values, replace with your desired colors
-        const backgroundColor = getRandomColor();
-        const textColor = getRandomColor();
-        const headingColor = getRandomColor();
+        // Fade in the GIF
+        welcomeGif.hide().fadeIn(750); // Adjust the duration (in milliseconds) as needed
 
-        // Update CSS styles
-        body.style.backgroundColor = backgroundColor;
-        body.style.color = textColor;
-        h1.style.color = headingColor;
-    }
-
-    function getRandomColor() {
-        // Generate a random hex color code
-        return "#" + Math.floor(Math.random()*16777215).toString(16);
-    }
+        // Wait for the GIF to finish playing (adjust the time accordingly)
+        setTimeout(function() {
+            // Fade out the GIF, fade out the loading screen, and show the rest of the webpage
+            welcomeGif.fadeOut(1000); // Adjust the duration (in milliseconds) as needed
+            welcomeSection.removeClass('show-gif');
+            welcomeSection.fadeOut(1000, function() {
+                // Optional: This callback function will be executed after fadeOut is complete
+                welcomeSection.hide();
+            });
+        }, 2500); // Example: 5000 milliseconds (5 seconds) for the GIF duration
+    })
 });
+
+// Function for Sidebar Tabs
+const tabs = document.querySelectorAll('[data-tab-value]')
+        const tabInfos = document.querySelectorAll('[data-tab-info]')
+ 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document
+        .querySelector(tab.dataset.tabValue);
+
+        tabInfos.forEach(tabInfo => {
+        tabInfo.classList.remove('active')
+        })
+        target.classList.add('active');
+    })
+})
+
+function scrollTimeline(direction) {
+    const timeline = document.getElementById('timeline-container');
+    const tweets = document.querySelectorAll('.tweet');
+    const tweetWidth = tweets[0].offsetWidth + 20; // Including margin
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = 0;
+    } else if (currentIndex >= tweets.length) {
+        currentIndex = tweets.length - 1;
+    }
+
+    const newPosition = -currentIndex * tweetWidth;
+    timeline.style.transform = translateX('${newPosition}px');
+}
+
+// Function for API profile picture
+const memesContainer = $("#memes-container");
+const changepfp = $("#changepfp");
+
+let memes = [];
+let currentMemeIndex = 0;
+
+fetchMemes();
+
+changepfp.click(changeMeme);
+
+function fetchMemes() {
+    $.ajax({
+        url: "https://api.imgflip.com/get_memes",
+        method: "GET",
+        dataType: "json",
+        success: function(data) {
+            memes = data.data.memes;
+            displayMeme(memes[currentMemeIndex]);
+        },
+        error: function(error) {
+            console.error("Error fetching memes:", error);
+        }
+    });
+}
+
+function displayMeme(meme) {
+    memesContainer.empty();
+
+    const memeElement = $("<div>").addClass("meme");
+    const imgElement = $("<img>").attr("src", meme.url).attr("alt", meme.name);
+
+    memeElement.append(imgElement);
+    memesContainer.append(memeElement);
+}
+
+function changeMeme() {
+    currentMemeIndex = (currentMemeIndex + 1) % memes.length;
+    displayMeme(memes[currentMemeIndex]);
+}
+
+// Tweets Tab 
+$("#save-button").on("click", function() {
+    saveNote();
+});
+
+function saveNote() {
+    // Get the input value
+    var note = $("#note-input").val().trim();
+
+    if (note === "") {
+      alert("Please enter a note.");
+      return;
+    }
+
+    // Get the ul element to display the notes
+    var notesList = $("#notes-list");
+
+    // Create a new li element with the note text
+    var li = $("<li>").text(note);
+
+    // Append the li element to the ul
+    notesList.append(li);
+
+    // Clear the input field
+    $("#note-input").val("");
+}
+
+// Elon Musk Tab
+$('#playButton').on('click', function () {
+    playSelection();
+});
+
+function playSelection() {
+    // Get the selected video
+    const selectedVideo = $('input[name="video"]:checked').val();
+
+    // Clear existing video players
+    $('.video-player').empty();
+
+    // Create a new video element
+    const videoElement = $('<video>').attr('controls', true);
+
+    // Set the source of the video based on the user's choice
+    if (selectedVideo === 'space') {
+        videoElement.attr('src', 'img/kitty.mp4');
+    } else if (selectedVideo === 'tesla') {
+        videoElement.attr('src', 'img/tweet.mp4');
+    }
+
+    // Append the video element to the container
+    $('.video-player').append(videoElement);
+}
